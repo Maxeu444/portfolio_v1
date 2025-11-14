5e026607ae628ebab1d6b4d79bf06b55fc332d21
Maxeu444 <139349788+Maxeu444@users.noreply.github.com>
2025-11-14 15:34:01 +0100

    Update ui/ux section project et ajout data

--- data/projects.ts
+++ data/projects.ts
@@ -8,20 +8,34 @@ export interface Project {
   title: string;
   description: string;
   stack: string[];
-  githubUrl: string;
+  githubUrl?: string;
+  websiteUrl?: string;
   highlighted?: boolean;
   type: 'scolaire' | 'pro' | 'perso';
+  teamType: 'solo' | 'equipe';
   date: string;
 }
 
 export const projects: Project[] = [
+  {
+    id: '1',
+    title: 'Cinefeel',
+    description: 'Site de recommandation de films et séries selon l\'humeur de l\'utilisateur.',
+    stack: ['Vue.js', 'NuxtJS', 'TypeScript', 'Java', 'Spring Boot'],
+    githubUrl: 'https://github.com/Maxeu444/Cinefeel_portfolio',
+    type: 'scolaire',
+    teamType: 'equipe',
+    date: '2023-2025',
+  },
   {
     id: '2',
     title: 'Mon portfolio',
     description: 'Mon portfolio personnel, sur lequel vous naviguez actuellement.',
     stack: ['Next.js', 'TypeScript', 'Tailwind CSS'],
     githubUrl: 'https://github.com/Maxeu444/portfolio_v1',
+    websiteUrl: 'https://maxhochart.fr',
     type: 'perso',
+    teamType: 'solo',
     date: '2025',
   },
   {
@@ -31,6 +45,7 @@ export const projects: Project[] = [
     stack: ['JavaScript'],
     githubUrl: 'https://github.com/Maxeu444/blockchain',
     type: 'perso',
+    teamType: 'solo',
     date: '2025',
   },
   {
@@ -40,6 +55,7 @@ export const projects: Project[] = [
     stack: ['VueJS', 'TypeScript', 'CSS', 'Mobile First'],
     githubUrl: 'https://github.com/Maxeu444/bar-app',
     type: 'scolaire',
+    teamType: 'solo',
     date: '2024',
   },
   {
@@ -49,15 +65,46 @@ export const projects: Project[] = [
     stack: ['Flutter', 'Firebase'],
     githubUrl: 'https://github.com/Maxeu444/nexusnow',
     type: 'scolaire',
+    teamType: 'solo',
     date: '2024',
   },
   {
     id: '6',
-    title: 'Projet Travel',
-    description: 'Site de reservation de voyages permettant aux utilisateurs de rechercher, réserver et gérer leurs voyages facilement. Développé dans le cadre de mon Titre Professionnel Développeur Web et Web Mobile.',
-    stack: ['Symfony', 'JavaScript', 'Twig', 'CSS'],
-    githubUrl: 'https://github.com/Maxeu444/ProjetTravel_FilRouge',
-    type: 'scolaire',
-    date: '2023',
+    title: 'Axens',
+    description: 'Axens est un entreprise de test produits, sur des panels de consommateurs. J\'ai participé au développement du tableau de bord "consommateur" ainsi qu\'à l\'interface administrateur.',
+    stack: ['Wordpress', 'PHP', 'HTML', 'CSS', 'API', 'Alternance Apresta'],
+    websiteUrl: 'https://axens-conseil.com/',
+    type: 'pro',
+    teamType: 'equipe',
+    date: '2023-2025',
   },
+  {
+    id: '7',
+    title: 'Workeez Connect',
+    description: 'Plateforme de recrutement par match affinitaire. Projet développé lors de mon alternance chez Apresta.',
+    stack: ['PHP', 'API', 'Alternance Apresta'],
+    websiteUrl: 'https://connect.workeez.fr/',
+    type: 'pro',
+    teamType: 'equipe',
+    date: '2025',
+  }, 
+  {
+    id: '8',
+    title: 'Bcyclet',
+    description: 'Site de location de vélos. Projet développé lors de mon alternance chez Apresta.',
+    stack: ['PHP', 'Wordpress', 'API', 'Alternance Apresta'],
+    websiteUrl: 'https://bcyclet.fr/',
+    type: 'pro',
+    teamType: 'equipe',
+    date: '2024-2025',
+  },
+  {
+    id: '9',
+    title: 'Outils Internes Apresta',
+    description: 'Développement d\'outils internes pour Apresta.',
+    stack: ['Laravel', 'React', 'API', 'Alternance Apresta'],
+    type: 'pro',
+    teamType: 'equipe',
+    date: '2025',
+  }
 ];
