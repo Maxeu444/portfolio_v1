74a73cd2764848f2c700b327d9ce3e73a3634d90
Max H <139349788+Maxeu444@users.noreply.github.com>
2025-11-17 15:11:34 +0100

    Update base URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://max-hochart.dev';
+  const baseUrl = 'https://maxhochart.fr';
   
   return {
     rules: [
